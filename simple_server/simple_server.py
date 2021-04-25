b23832b12b5f4c262b1c168a6a2b43a795557631
dima <hexdump1337@gmail.com>
2021-04-25 17:40:58 +0300

    resources added

--- simple_server/simple_server.py
+++ simple_server/simple_server.py
@@ -10,12 +10,12 @@ class ServerHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
+    
     def do_GET(self):
         logging.info("GET request!\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
         self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-
+    
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
@@ -36,7 +36,7 @@ def run(server_class = HTTPServer, handler_class = ServerHandler, port = 8080):
     logging.info('stopping custom server...\n')
 
 if __name__ == '__main__':
-   if len(argv) == 2:
-      run(port = int(argv[1]))
-   else:
-      run()
+    if len(argv) == 2:
+        run(port = int(argv[1]))
+    else:
+        run()
